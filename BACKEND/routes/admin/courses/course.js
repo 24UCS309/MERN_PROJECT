d3bc50dee5f770230f53e5908c0af5401f95057e
Runali Ramdurge <runaliramdurge2005@gmail.com>
2025-12-30 22:34:38 +0530

    updated course.js

--- BACKEND/routes/admin/courses/course.js
+++ BACKEND/routes/admin/courses/course.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
+router.post("/addCourse", (req, res) => {
   const {
     course_name,
     description,
@@ -24,17 +24,10 @@ router.post("/", (req, res) => {
     end_date,
     video_expire_days,
   } = req.body;
-  const sql = `insert into courses(course_name, description, fees, start_date, end_date,video_expire_days) values(?,?,?,?,?,?,?)`;
+  const sql = `insert into courses(course_name, description, fees, start_date, end_date,video_expire_days) values(?,?,?,?,?,?)`;
   pool.query(
     sql,
-    [
-      course_name,
-      description,
-      fees,
-      start_date,
-      end_date,
-      video_expire_days,
-    ],
+    [course_name, description, fees, start_date, end_date, video_expire_days],
     (error, data) => {
       res.send(result.createResult(error, data));
     }
